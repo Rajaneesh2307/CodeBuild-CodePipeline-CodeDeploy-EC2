b992b254cce54ad15a465e8c71f7c8a6d575855d
Yembuluru Rajaneesh <101640141+Rajaneesh2307@users.noreply.github.com>
2025-06-02 11:56:07 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Copying app files to Nginx directory..."
+
+# Ensure the Nginx directory exists
+mkdir -p /var/www/html/
+
+# Optionally clear the directory (uncomment if needed)
+rm -rf /var/www/html/*
+
+# Copy React Vite build files to Nginx directory
 cp -r /home/ec2-user/react-vite-app/* /var/www/html/
