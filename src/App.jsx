c3aa55455e030ed0f72fd78bf8c6e7b0898a4f6b
Yembuluru Rajaneesh <101640141+Rajaneesh2307@users.noreply.github.com>
2025-08-28 16:15:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <h3>Yembuluru Rajaneesh</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 7)}>
+        <button onClick={() => setCount((count) => count + 5)}>
           count is {count}
         </button>
         <p>
